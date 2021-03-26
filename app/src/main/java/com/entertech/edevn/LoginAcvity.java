c17f1c641480626e06a3032afefd73c0ac87b2d1
Your Name <you@example.com>
2021-03-27 00:56:52 +0600

    roboto font download and set

--- app/src/main/java/com/entertech/edevn/LoginAcvity.java
+++ app/src/main/java/com/entertech/edevn/LoginAcvity.java
@@ -1,14 +1,21 @@
 package com.entertech.edevn;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginAcvity extends AppCompatActivity {
 
+    private Button loginContinueButton;
+    private EditText loginEmailOrPhoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_acvity);
+
+        loginContinueButton = (Button) findViewById(R.id.login_continue_btn_id);
+        loginEmailOrPhoneNumber = (EditText)  findViewById(R.id.login_email_or_phone_number_id);
     }
 }
