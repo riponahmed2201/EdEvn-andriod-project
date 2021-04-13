2437b20a90685d54da8e8d5f23c0cccb3fcf4749
ripon <riponahmed2201@gmail.com>
2021-04-14 03:35:57 +0600

    live class details activity issue done

--- app/src/main/java/com/entertech/edevn/HomeActivity.java
+++ app/src/main/java/com/entertech/edevn/HomeActivity.java
@@ -1,14 +1,28 @@
 package com.entertech.edevn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private CardView liveClassDetailsId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        liveClassDetailsId = findViewById(R.id.live_class_details_Id);
+
+        liveClassDetailsId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, LiveClassActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/entertech/edevn/LiveClassActivity.java
+++ app/src/main/java/com/entertech/edevn/LiveClassActivity.java
@@ -2,13 +2,28 @@ package com.entertech.edevn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LiveClassActivity extends AppCompatActivity {
 
+    private ImageView liveClassBackId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_class);
+
+        liveClassBackId = findViewById(R.id.live_class_back_Id);
+
+        liveClassBackId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LiveClassActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
