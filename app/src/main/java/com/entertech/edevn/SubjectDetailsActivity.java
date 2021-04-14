169850c624babc1f203b234e9e6c180cf8baceb1
ripon <riponahmed2201@gmail.com>
2021-04-14 16:45:20 +0600

    home activity start quize button issue fix

---
+++ app/src/main/java/com/entertech/edevn/SubjectDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.entertech.edevn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubjectDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_details);
+    }
+}
