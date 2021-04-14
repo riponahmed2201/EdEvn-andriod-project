6bfc5c4d1f00609c54c451bb2c40aa9f92230cd3
ripon <riponahmed2201@gmail.com>
2021-04-14 23:59:13 +0600

    subject details page design done

--- app/src/main/java/com/entertech/edevn/SubjectDetailsActivity.java
+++ app/src/main/java/com/entertech/edevn/SubjectDetailsActivity.java
@@ -2,13 +2,27 @@ package com.entertech.edevn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SubjectDetailsActivity extends AppCompatActivity {
 
+    private ImageView subjectDetailsBackId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_details);
+
+        subjectDetailsBackId = findViewById(R.id.subject_details_back_Id);
+
+        subjectDetailsBackId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SubjectDetailsActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
