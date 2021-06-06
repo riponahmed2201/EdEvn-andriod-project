a5b071c4d2164c3ba203346d3c939e194897b874
ripon <riponahmed2201@gmail.com>
2021-06-06 18:27:55 +0600

    live class activity design done

--- app/src/main/java/com/entertech/edevn/LiveClassActivity.java
+++ app/src/main/java/com/entertech/edevn/LiveClassActivity.java
@@ -1,14 +1,28 @@
 package com.entertech.edevn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LiveClassActivity extends AppCompatActivity {
 
+    private CardView liveClassDetailsId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_class);
+
+        liveClassDetailsId = findViewById(R.id.live_class_details_Id);
+
+        liveClassDetailsId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LiveClassActivity.this,FreeLiveClassActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
