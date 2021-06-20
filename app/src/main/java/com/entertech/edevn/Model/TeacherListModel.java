258f454bae347cf9221400d789cf7b72cd06bf6e
Ripon <ripon@pop-os.localdomain>
2021-06-20 21:54:55 +0600

    live class teacher list adapter

--- app/src/main/java/com/entertech/edevn/Model/TeacherListModel.java
+++ app/src/main/java/com/entertech/edevn/Model/TeacherListModel.java
@@ -2,4 +2,26 @@ package com.entertech.edevn.Model;
 
 public class TeacherListModel {
 
+    private String teacherName, teacherImage;
+
+    public TeacherListModel(String teacherName, String teacherImage) {
+        this.teacherName = teacherName;
+        this.teacherImage = teacherImage;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public String getTeacherImage() {
+        return teacherImage;
+    }
+
+    public void setTeacherImage(String teacherImage) {
+        this.teacherImage = teacherImage;
+    }
 }
